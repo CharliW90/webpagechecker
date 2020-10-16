4fd5888527f14ebd202fd34976a834f9efccdc57
ArcadiusGames <72148151+ArcadiusGames@users.noreply.github.com>
2020-10-16 12:14:14 +0100

    Update pagecheck.py

--- pagecheck.py
+++ pagecheck.py
@@ -5,20 +5,27 @@ import smtplib
 from email.message import EmailMessage
 import hashlib
 from urllib.request import urlopen
+from requests_html import HTMLSession
 
 url = os.environ.get("pagetocheck")
-response = urlopen(url).read()
-currentHash = hashlib.sha224(response).hexdigest()
+session = HTMLSession()
+response = session.get(url)
+shop = response.html.xpath(/html/body/div[1]/main/article/section[2]/div[2]/div/section/div[2])
+print(shop)
+currentHash = hashlib.sha224(shop).hexdigest() 
 
 while True:
 
     try:
 
-        response = urlopen(url).read()
-        currentHash = hashlib.sha224(response).hexdigest()
+        session = HTMLSession()
+        response = session.get(url)
+        shop = response.html.xpath(/html/body/div[1]/main/article/section[2]/div[2]/div/section/div[2])
+        print(shop)
+        currentHash = hashlib.sha224(shop).hexdigest()
         time.sleep(30)
-        response = urlopen(url).read()
-        newHash = hashlib.sha224(response).hexdigest()
+        shop = response.html.xpath(/html/body/div[1]/main/article/section[2]/div[2]/div/section/div[2])
+        newHash = hashlib.sha224(shop).hexdigest()
 
         if newHash == currentHash:
             continue
