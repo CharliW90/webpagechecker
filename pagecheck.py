c0cfe6f03604d035f1fd9c8f29f8a4e1c2123e83
ArcadiusGames <72148151+ArcadiusGames@users.noreply.github.com>
2020-10-16 13:30:39 +0100

    Update pagecheck.py

--- pagecheck.py
+++ pagecheck.py
@@ -14,7 +14,7 @@ shop = page.find('div', {"class": "products-flex-container"})
 snapshot = str(shop)
 currentHash = hashlib.sha224(snapshot.encode('utf-8')).hexdigest()
 print("successfully hashed snapshot of shop")
-print("snapshot: " + snaphsot)
+print("snapshot: " + snapshot)
 print("Hash: " + currentHash)
         
 while True:
